dd4508dce7ab6c40896e884541ff8616dc16520b
Valefar Frontend Developer <inzhener.intellect@gmail.com>
2019-04-21 09:42:44 +0700

    Add files via upload

---
+++ precache-manifest.738f59d0f77bf9ba2f44eeff64dd1da3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "caed0a3d2eff3a0ebeba",
+    "url": "/simpleChat/static/css/main.a49738f3.chunk.css"
+  },
+  {
+    "revision": "caed0a3d2eff3a0ebeba",
+    "url": "/simpleChat/static/js/main.caed0a3d.chunk.js"
+  },
+  {
+    "revision": "12b2cd54308bdfb4bee2",
+    "url": "/simpleChat/static/js/1.12b2cd54.chunk.js"
+  },
+  {
+    "revision": "df993f0677803ceef844",
+    "url": "/simpleChat/static/js/runtime~main.df993f06.js"
+  },
+  {
+    "revision": "249e3c7b9a2e0ffa74328f5eaa17e974",
+    "url": "/simpleChat/index.html"
+  }
+];
